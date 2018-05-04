6e60de5b549cd8bb96786b787b86267df8850b28
adascalu <alexandru.dascalu@adswizz.com>
2018-05-04 16:58:41 +0300

    added attributes to candidate

--- src/main/java/domain/Candidate.java
+++ src/main/java/domain/Candidate.java
@@ -4,34 +4,66 @@ package domain;
 public class Candidate {
 
     private String name;
+    private String job;
     private String email;
-    private String templateName;
+    private String phone;
+    private String info;
+    private String link;
 
-    public String getJobLink() {
-        return jobLink;
+    public String getLink() {
+        return link;
     }
 
-    private String jobLink;
+    public void setLink(String link) {
+        this.link = link;
+    }
 
+    public Candidate(String name, String job, String email, String phone, String info, String link) {
+        this.name = name;
+        this.job = job;
+        this.email = email;
+        this.phone = phone;
+        this.info = info;
+        this.link = link;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
 
     public String getEmail() {
         return email;
     }
 
-
-    public String getTemplateName() {
-        return templateName;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public Candidate(String name, String email, String templateName, String jobLink) {
-        this.name = name;
-        this.email = email;
-        this.templateName = templateName;
-        this.jobLink = jobLink;
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
