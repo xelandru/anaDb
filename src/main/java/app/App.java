d844e2ae0e970f1281724cde57637f3817d99586
adascalu <alexandru.dascalu@adswizz.com>
2018-05-05 17:57:48 +0300

    added derby docker and driver

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,25 +1,9 @@
 package app;
 
 
-import domain.Candidate;
-import templates.Template;
-import templates.TemplateA;
-import templates.TemplateFactory;
-
 public class App {
 
 
     public static void main(String[] args) {
-        Candidate  c1 = new Candidate("Gigel", "gigel@gmail.com", "template_A.txt",  "www.job.com");
-        Candidate  c2 = new Candidate("Gigel", "gigel@gmail.com", "template_B.txt",  "www.job.com");
-
-
-        String msg1 = TemplateFactory.getTemplate(c1).getCustomMessage();
-        String msg2 = TemplateFactory.getTemplate(c2).getCustomMessage();
-
-        System.out.println(msg1);
-        System.out.println(msg2);
-
     }
-
 }
---
+++ src/main/resources/docker/startDerby.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker pull az82/docker-derby
+docker run --name my_derby az82/docker-derby:latest
