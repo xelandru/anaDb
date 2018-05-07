031af2567082f0bce6e1327e2692d9a590c4b004
adascalu <alexandru.dascalu@adswizz.com>
2018-05-07 17:46:30 +0300

    docker for derby

---
+++ src/main/resources/docker/create_db.sql
@@ -0,0 +1,8 @@
+connect 'jdbc:derby:MyDbTest;create=true' ;
+CREATE table derbyDB (num int, addr varchar (40)) ;
+INSERT INTO derbyDB VALUES (
+                           1956,
+                           'Webster St.');
+INSERT INTO derbyDB VALUES (
+                           1910,
+                           'Union St.');
